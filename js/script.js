c43bca931c15d90f0e7ab330b244965466222ac1
mdriajul01 <mdriajulislam9026@gmail.com>
2024-08-18 19:21:59 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// typing Script JS
+var typed = new Typed(".typing",{
+    strings: ["Web Designer","Web Developer",
+        "socail Marketier","Blogger","Data Analysised"],
+    typeSpeed:80,
+    backSpeed:80,
+})
+var typed = new Typed(".typing-2",{
+    strings: ["Web Designer","Web Developer",
+        "socail Marketier","Blogger","Data Analysised"],
+    typeSpeed:100,
+    backSpeed:60,
+    
+})
